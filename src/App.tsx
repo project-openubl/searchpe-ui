14f41042dd8566c3cf292a3dc772692593e52c3d
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-01-16 15:19:38 +0100

    First template

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { Routes } from './Routes';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
+import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <Routes childProps={this.props} />
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default withRouter<any>(connect()(App));
+export default App;
