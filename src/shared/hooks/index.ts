0f92c20475b1725499efbce23dd9f8ed946d5f00
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2021-01-17 16:33:25 +0100

    add list contribuyentes (#7)

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,4 +1,5 @@
 export { useDeleteVersion } from "./useDeleteVersion";
+export { useFetchContribuyentes } from "./useFetchContribuyentes";
 export { useFetchVersions } from "./useFetchVersions";
 export { useTableControls } from "./useTableControls";
 export { useTableControlsOffline } from "./useTableControlsOffline";
