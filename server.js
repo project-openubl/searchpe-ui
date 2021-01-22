ea10bfba085247f77297be40243249d1cec1284c
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-01-22 21:03:51 +0100

    change dockerfile

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express(), bodyParser = require("body-parser");
 
+const setupProxy = require('./src/setupProxy');
+
 port = 3000;
+
+setupProxy(app);
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "build")));
