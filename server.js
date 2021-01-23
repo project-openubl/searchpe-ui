472d76baea6878db5577545ee7bc84aa6dc07333
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-01-23 09:51:18 +0100

    Enhance nodejs server starter

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
-const app = express(), bodyParser = require("body-parser");
+const app = express(),
+  bodyParser = require("body-parser");
 
-const setupProxy = require('./src/setupProxy');
+const setupProxy = require("./src/setupProxy");
 
 port = 3000;
 
@@ -10,6 +11,11 @@ setupProxy(app);
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "build")));
+
+// Handles any requests that don't match the ones above
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/build/index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server listening on the port::${port}`);
