8d5a1fe59b0ad1652377c3ffda7db649cffdf930
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2021-01-30 13:26:52 +0100

    Change env parameter nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-if [[ -z "$API_URL" ]]; then
-  echo "You must provide API_URL environment variable" 1>&2
+if [[ -z "$SEARCHPE_API_URL" ]]; then
+  echo "You must provide SEARCHPE_API_URL environment variable" 1>&2
   exit 1
 fi
 
 if [ -f ./nginx.conf.template ]; then
   echo "---> Processing nginx.conf.template configuration file..."
-  envsubst '${API_URL}' < ./nginx.conf.template > ./nginx.conf
+  envsubst '${SEARCHPE_API_URL}' < ./nginx.conf.template > ./nginx.conf
   cp -v ./nginx.conf "${NGINX_CONF_PATH}"
   rm -f ./nginx.conf
 fi
